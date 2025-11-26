21de5c032a7b7ab21c0c22aa3f78293dde011744
Anderson Dias <andy.skan10@gmail.com>
2025-11-26 18:13:18 -0300

    continuação do pokedex

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_image');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+
 
 const fetchPokemon =  async (pokemon) => {
 
-    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
 
     const data = await APIresponse.json();
 
@@ -18,7 +21,20 @@ const renderPokemon = async (pokemon) => {
 
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data.sprites.front_default;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+    input.value = '';
+
 }
-renderPokemon('274');
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    renderPokemon(input.value);
+    
+
+
+
+
 
