76f42778a59d81d820271e9b392c131a850f0f05
Anderson Dias <andy.skan10@gmail.com>
2025-11-26 15:25:09 -0300

    feito o começo da criação da pokedex

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+
+const fetchPokemon =  async (pokemon) => {
+
+    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    const data = await APIresponse.json();
+
+   return data;
+
+
+}
+
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data.sprites.front_default;
+}
+renderPokemon('274');
+
