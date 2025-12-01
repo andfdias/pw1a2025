2e6a98f5328b9c9afd9132a46e43e8f93208bccb
Anderson Dias <andy.skan10@gmail.com>
2025-12-01 11:33:39 -0300

    feito a troca de cores da pokedex

--- js/script.js
+++ js/script.js
@@ -9,15 +9,21 @@ const fetchPokemon =  async (pokemon) => {
 
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
 
-    const data = await APIresponse.json();
+   
 
-   return data;
+    if (APIresponse.status == 200){
+        
 
-
+    const data = await APIresponse.json();  
+    return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'loading...';
     const data = await fetchPokemon(pokemon);
+
+    if (data) {
 
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
@@ -25,14 +31,23 @@ const renderPokemon = async (pokemon) => {
 
     input.value = '';
 
+    } else {
+        pokemonName.innerHTML = 'Not found :c';
+        pokemonName.innerHTML = '';
+
+    }
 }
 
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    
+
     renderPokemon(input.value);
     
+ 
+});
+
+renderPokemon('1')
 
 
 
